31e4796a1b0ed4915f9c4eeca4dfc41302e3a60e
Christian Presley <presleyd@ibbr.umd.edu>
2022-06-07 12:36:21 -0400

    Allow to detect http or https before redirect

--- www/index.php
+++ www/index.php
@@ -2,7 +2,14 @@
 
 // https://stackoverflow.com/a/21129068
 
-$url = 'http://' . $_SERVER['HTTP_HOST'];            // Get the server
+if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+    $protocol = "https://";
+}
+else {
+    $protocol = "http://";
+}
+
+$url = $protocol . $_SERVER['HTTP_HOST'];            // Get the server
 $url .= rtrim(dirname($_SERVER['PHP_SELF']), '/\\'); // Get the current directory
 $url .= '/web/index.php';
 header('Location: ' . $url, true, 302);              // Use either 301 or 302
